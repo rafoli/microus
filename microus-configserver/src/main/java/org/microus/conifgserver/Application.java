1e09407c719fa92db229dc12eaacb9ead8e84490
Rafael Oliveira <rafael.oliveira@mv.com.br>
2016-01-12 18:15:52 -0300

    Added configserver main class

---
+++ microus-configserver/src/main/java/org/microus/conifgserver/Application.java
@@ -0,0 +1,10 @@
+package org.microus.conifgserver;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+ 
