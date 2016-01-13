795b2e7e85b2287493432212ed60695e917741e4
Rafael Oliveira <rafael.oliveira@mv.com.br>
2016-01-13 17:50:11 -0300

    Added apigateway microservice

---
+++ microus-apigateway/src/main/java/org/microus/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package org.microus.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableZuulProxy
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
